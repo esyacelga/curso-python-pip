02a85c0753192562e89785373a15b14286dfc66f
Santiago Yacelga <esyacelga@hotmail.com>
2024-01-31 23:19:24 -0500

    Correcciones

--- grafica/utils_lector.py
+++ grafica/utils_lector.py
@@ -3,6 +3,17 @@ def get_population_by_country(data,country):
    result = list(filter(lambda item:item['Country/Territory']==country,data))
    return result
 
+def get_filter_population_by_continent(data,continent):
+   result = list(filter(lambda item:item['Continent']==continent,data))
+   return result
+
+
+
+def get_values_by_colum (data, columna):
+    result = list(map(lambda item:item[columna],data))
+    return result
+    
+
 def get_poulation_keys(data):
     objecto = data[0]
     try:
@@ -25,9 +36,9 @@ def generate_bar_char(labels, values):
 def generate_pie_char(labels, values):
     try:
         fig, ax = plt.subplots()
-        ax.pie(labels,values)
+        ax.pie(values,labels=labels)
         ax.axis('equal')    
-        plt.savefig ('grafico.png')
+        plt.savefig ('grafico-pie.png')
     except  Exception as e:
          print(f"Error personalizado capturado: {e}")         
             
--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from grafica import utils_lector as ulc
 def lectura():
     path= './grafica/data.csv'
     data = lc.readCsv(path)
-    country = input("Ingrese el pais===>")
+    data = ulc.get_filter_population_by_continent(data,'South America')
+    value_x = ulc.get_values_by_colum(data,'Country/Territory')
+    value_y = ulc.get_values_by_colum(data,'World Population Percentage')
+    ulc.generate_pie_char(value_x,value_y)
+    """ country = input("Ingrese el pais===>")
     pais_filtrado = ulc.get_population_by_country(data,country)
     labels, values = ulc.get_poulation_keys(pais_filtrado)
     ulc.generate_bar_char(labels, values)
     print(labels, values)
-    
+     """
 
 if __name__ == '__main__':
     lectura()
